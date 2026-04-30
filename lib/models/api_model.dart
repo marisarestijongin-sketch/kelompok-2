4ce221626ccb026d0d4def48d8d5943e600b1a0f
berliyana14 <berliyanaberliyana3@gmail.com>
2026-04-30 16:06:24 +0700

    up berli

--- lib/models/api_model.dart
+++ lib/models/api_model.dart
@@ -1,19 +1,24 @@
+import 'dart:convert';
+
+ApiModel apiModelFromJson(String str) =>
+    ApiModel.fromJson(json.decode(str));
+
 class ApiModel {
-  final bool? status;
-  final String? message;
-  final dynamic data;
+  final String status;
+  final String message;
+  final String title;
 
   ApiModel({
-    this.status,
-    this.message,
-    this.data,
+    required this.status,
+    required this.message,
+    required this.title,
   });
 
   factory ApiModel.fromJson(Map<String, dynamic> json) {
     return ApiModel(
-      status: json['status'],
-      message: json['message'],
-      data: json['data'],
+      status: json['status'] ?? "",
+      message: json['message'] ?? "",
+      title: json['data']?['title'] ?? "No Title",
     );
   }
 }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,19 +1,22 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/api_model.dart';
 
 class ApiService {
-  static const String url =
-      "https://api.ppb.widiarrohman.my.id/api/2026/uts/A/kelompok2/check";
-
   static Future<ApiModel> getData() async {
-    final response = await http.get(Uri.parse(url));
+    final response = await http.get(
+      Uri.parse(
+        'https://api.ppb.widiarrohman.my.id/api/2026/uts/A/kelompok2/check',
+      ),
+    );
 
     if (response.statusCode == 200) {
-      final jsonData = jsonDecode(response.body);
-      return ApiModel.fromJson(jsonData);
+      return ApiModel(
+        status: "success",
+        message: "OK",
+        title: response.body, // 🔥 ambil langsung text
+      );
     } else {
-      throw Exception("Gagal ambil data: ${response.statusCode}");
+      throw Exception('Failed to load data');
     }
   }
 }
